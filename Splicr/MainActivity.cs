da53a6d16fa60e613a472d156078b4f4b76a9ba5
Chance Cooper <c0659824@mylambton.ca>
2017-04-04 08:37:34 -0400

    Add project files.

---
+++ Splicr/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace Splicr
+{
+    [Activity(Label = "Splicr", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Window.RequestFeature(WindowFeatures.NoTitle);
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
